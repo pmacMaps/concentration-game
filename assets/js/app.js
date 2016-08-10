2727bf23cdd3f4e5d9256bfab2fc37e7b4fef77d
Patrick McKinney <pnmcartography@gmail.com>
2016-08-09 21:11:42 -0400

    add code to try to remove cards on match

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,12 +22,12 @@ function shuffle(array) {
 
 // initial array of images
 var photosCollage = [
+    '/concentration-game/assets/images/img-0.jpg',
     '/concentration-game/assets/images/img-1.jpg',
     '/concentration-game/assets/images/img-2.jpg',
     '/concentration-game/assets/images/img-3.jpg',
     '/concentration-game/assets/images/img-4.jpg',
-    '/concentration-game/assets/images/img-5.jpg',
-    '/concentration-game/assets/images/img-6.jpg'
+    '/concentration-game/assets/images/img-5.jpg'
 ];
 
 // first randomized array of images
@@ -40,16 +40,18 @@ var rowContainer = $('#rowContainer');
 
 // add first random array of images to container
 for (var i = 0; i < photosArray1.length; i++) {
-    rowContainer.append('<div class="col-md-3 card-container"><div class="top"></div><img class="bottom" alt="mckinney family" src="' + photosArray1[i] + '" /></div>');    
+    rowContainer.append('<div class="img-' + i.toString() + ' col-md-3 card-container"><div class="top"></div><img class="bottom" alt="mckinney family" src="' + photosArray1[i] + '" /></div>');    
 }
 
 // add second random array of images to container
 for (var i = 0; i < photosArray2.length; i++) {
-    rowContainer.append('<div class="col-md-3 card-container"><div class="top"></div><img class="bottom" alt="mckinney family" src="' + photosArray1[i] + '" /></div>');     
+    rowContainer.append('<div class="img-' + i.toString() + ' col-md-3 card-container"><div class="top"></div><img class="bottom" alt="mckinney family" src="' + photosArray1[i] + '" /></div>');     
 }     
 
 var cardContainer = $('.card-container');
 var displayedImages = [];
+var elementList = [];
+
 
 // Only want 2 images in the array at a time
 // only want to add an image (run a function) if the image display is set to block
@@ -77,14 +79,42 @@ function addImgToArray(img) {
 function testImageFileName() {
     if (displayedImages.length === 2) {
         if (displayedImages[0] === displayedImages[1]) {
-            console.log('images match. Do something cool!');
+            //console.log('images match. Do something cool!');
             // add a score or message in footer or header?
+            var targetClass = displayedImages[0].split('/');
+            var newTest = targetClass.pop();
+            var target = newTest.split('.');
+            var result = target[0];
+            var result2 = '.' + result + ' .col-md-3 .card-container';
+            
+            console.log(targetClass);
+            console.log(newTest);
+            console.log(result);
+            console.log(result2);
+            
+            $(result2).css('display', 'none');
+            
+            //mask.css('background', '#fff');
+            //mask.css('display', 'block');
+            
+            //console.log(targetElements);
             emptyImgArray();
+            
+            
+            
+            /*
+            if ($('.card-container').hasClass(result)) {
+                $(result2).css('display', 'none');
+            }
+            */
+            
+            
         } else {
             console.log('images do not match. Sorry!');
             // toggle display of images that are displayed
             // toggle their sibling as well
             emptyImgArray();
+            
         }
     } else {
         console.log('Length of image array is ' + displayedImages.length);
@@ -95,7 +125,9 @@ function testImageFileName() {
 cardContainer.click(function() {
     var img = $(this).children('.bottom');
     var mask = $(this).children('.top');
-   
+    var _this = $(this);
+    
+           
     // toggle display for img
     img.toggle();
    
@@ -103,11 +135,11 @@ cardContainer.click(function() {
    // if not, do nothing (log message for testing)    
    if (img.css('display') === 'block') {
        addImgToArray(img);
-       testImageFileName();
+       testImageFileName()
     } else {
       console.log('img display property is ' + img.css('display'));
     }
     
     // toggle display for mask
-    mask.fadeToggle();    
+    mask.fadeToggle();     
 });
